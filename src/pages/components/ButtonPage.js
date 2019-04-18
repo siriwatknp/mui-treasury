9129f08cdcd1c6e6c25fdcaa180ffb43703eb552
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2019-04-18 15:56:21 +0700

    add predefined button

--- src/pages/components/ButtonPage.js
+++ src/pages/components/ButtonPage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import merge from 'lodash/merge';
 import get from 'lodash/get';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { Box } from 'components/atoms';
 import { PreviewWidget } from 'components/molecules';
 import ShouldUpdate from 'containers/ShouldUpdate';
+import ButtonSection from 'pages/components/predefined/ButtonSection';
 import ShadowlessButton from 'components/buttons/ShadowlessButton';
 import ChubbyButton from 'components/buttons/ChubbyButton';
 import GithubButton from 'components/buttons/GithubButton';
@@ -113,6 +117,39 @@ const ButtonPage = ({ counter, globalTheme, onSelectComponent }) => (
       sm: 4,
     }}
   >
+    <Typography variant={'h5'}>
+      Predefined <code>Button</code>
+    </Typography>
+    <Typography gutterBottom>
+      The one and only one <code>Button</code> that you need if you are using
+      Material-UI, just copy the snippet.
+    </Typography>
+    <Typography>
+      <Link href={'https://codesandbox.io/s/4wmz707on4'}>Code Sandbox</Link>
+    </Typography>
+    <Box pt={{ xs: 2, sm: 4 }}>
+      <ButtonSection globalTheme={globalTheme} />
+    </Box>
+    <Typography variant={'h6'}>How to use</Typography>
+    <Typography>
+      1. Copy component in <code>/src/components/predefined/Button.js</code>
+    </Typography>
+    <Typography>2. Use this component instead of Material-ui Button</Typography>
+    <Typography gutterBottom>
+      3. Look at component propTypes, adjust default props to suit your
+      project.
+    </Typography>
+    <Box pb={2} />
+    <Divider />
+    <Box pb={2} />
+    <Typography variant={'h5'}>
+      Showcase <code>Button</code>
+    </Typography>
+    <Typography gutterBottom>
+      This section shows that Material-UI can be customize to many themes as you
+      like.
+    </Typography>
+    <Box pb={2} />
     <Grid container spacing={32}>
       {components.map(({ component, render, previewProps }) => (
         <Grid key={get(component, 'metadata.name')} item xs={12} sm={6} lg={4}>
