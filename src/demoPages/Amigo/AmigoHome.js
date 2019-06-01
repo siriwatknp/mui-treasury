8e9562550bce790d25351866baecf2385f3e70bf
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2019-06-01 10:41:18 +0700

    add oswald font

--- src/demoPages/Amigo/AmigoHome.js
+++ src/demoPages/Amigo/AmigoHome.js
@@ -10,7 +10,7 @@ import theme from './theme';
 
 const url =
   // eslint-disable-next-line max-len
-  'https://fonts.googleapis.com/css?family=Poppins:200,400,500,700,900&display=swap';
+  'https://fonts.googleapis.com/css?family=Poppins:200,400,500,700,900|Oswald:400,700&display=swap';
 
 const AmigoHome = () => {
   const renderParagraph = () => (
