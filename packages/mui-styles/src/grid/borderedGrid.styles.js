aa5f45759c2d036f45f062a79716301730b41427
siriwatknp <siriwatkunaporn@gmail.com>
2019-08-20 21:07:50 +0700

    add grid stories

--- packages/mui-styles/src/grid/borderedGrid.styles.js
+++ packages/mui-styles/src/grid/borderedGrid.styles.js
@@ -1,21 +1,33 @@
 import Color from 'color';
 
-export default ({ breakpoints, palette }) => ({
-  item: ({ colWidth = {}, borderColor }) => {
-    let color = Color(palette.primary.main)
-      .fade(0.9)
-      .toString();
-    if (borderColor && typeof borderColor === 'string') {
-      if (borderColor === 'divider' || borderColor.includes('.')) {
-        color = palette;
-        const path = borderColor.split('.');
-        path.forEach(key => {
-          color = color[key];
-        });
-      } else {
-        color = borderColor;
-      }
+const getColor = (palette, borderColor) => {
+  let color = Color(palette.primary.main)
+    .fade(0.9)
+    .toString();
+  if (borderColor && typeof borderColor === 'string') {
+    if (borderColor === 'divider' || borderColor.includes('.')) {
+      color = palette;
+      const path = borderColor.split('.');
+      path.forEach(key => {
+        color = color[key];
+      });
+    } else {
+      color = borderColor;
     }
+  }
+  return color;
+};
+
+export default ({ breakpoints, palette }) => ({
+  container: ({ borderColor }) => {
+    const color = getColor(palette, borderColor);
+    return {
+      border: `1px solid ${color}`,
+      borderBottom: 'none',
+    };
+  },
+  item: ({ colWidth = {}, borderColor }) => {
+    const color = getColor(palette, borderColor);
     const screens = Object.keys(colWidth);
     return {
       boxShadow: `0 1px 0 0 ${color}`,
@@ -23,6 +35,7 @@ export default ({ breakpoints, palette }) => ({
       ...screens.reduce((result, key, index) => {
         const media = {};
         const width = colWidth[key];
+        console.log('width', width);
         const { up, only } = breakpoints;
         const creator = index !== screens.length - 1 ? only : up;
         media[creator(key)] = {
---
+++ src/__stories__/grid.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import { text, boolean, color, object } from '@storybook/addon-knobs';
+import { useBorderedGrid } from '@mui-treasury/styles';
+
+import createContainer from './containerDecorator';
+
+const StylesProvider = ({ useStyles, children }) => {
+  const styles = useStyles();
+  return children(styles);
+};
+
+storiesOf('Components/Grid', module)
+  .addDecorator(createContainer({ maxWidth: 'xs' }))
+  .add('bordered', () => {
+    const colWidth = object('colWidth', { xs: 6, sm: 4, md: 3 });
+    const useStyles = () =>
+      useBorderedGrid({
+        colWidth,
+        borderColor: color('color', undefined),
+      });
+    return (
+      <StylesProvider useStyles={useStyles}>
+        {styles => (
+          <Box py={'2.5rem'}>
+            <Grid container classes={styles}>
+              {new Array(12).fill(0).map((item, index) => (
+                <Grid key={index} item classes={styles} {...colWidth}>
+                  <Box minHeight={'10rem'} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        )}
+      </StylesProvider>
+    );
+  });
