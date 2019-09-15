d093d891a980a08b9e5b785475e23863305b278a
Mike Staub <mikestaub22@gmail.com>
2019-09-14 20:51:19 -0700

    refactor PeaGroupDialog component

--- src/brands/peapods/lib/PeaGroupDialog.js
+++ src/brands/peapods/lib/PeaGroupDialog.js
@@ -1,17 +1,30 @@
-import TextField from '@material-ui/core/TextField/TextField';
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField/TextField';
+import CardMedia from '@material-ui/core/CardMedia';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import FormLabel from '@material-ui/core/FormLabel';
+import Box from '@material-ui/core/Box';
+
 import PeaDialog from './PeaDialog';
 import PeaButton from './PeaButton';
-import PeaRadioGroup from './PeaRadioGroup';
 import PeaLoadingSpinner from './PeaLoadingSpinner';
+import PeaIcon from './PeaIcon';
+
+// TODO: render group members
 
 const PeaGroupDialog = ({
   dialogTitle,
   actionText,
   name,
+  description,
   type,
+  inviteInput,
+  typeInput,
+  profilePhoto,
   submitting,
+  onChangeCoverPhotoClicked,
   onChange,
   onClose,
   onSave,
@@ -22,46 +35,58 @@ const PeaGroupDialog = ({
     title={dialogTitle}
     titleVariant={'secondaryCentered'}
     content={
-      <React.Fragment>
+      <>
+        <FormLabel component="legend">Cover Picture</FormLabel>
+
+        <Box
+          style={{
+            marginTop: 16,
+            marginBottom: 16,
+          }}
+        >
+          <CardMedia
+            className={'MuiCardMedia-root'}
+            image={profilePhoto}
+            style={profilePhoto && { border: 'none' }}
+            onClick={onChangeCoverPhotoClicked}
+          >
+            <ButtonBase className={'PeaGroup-coverImgBtn'}>
+              <PeaIcon
+                inverted
+                icon={'add'}
+                shape={'square'}
+                style={{ fontSize: 100 }}
+              />
+            </ButtonBase>
+          </CardMedia>
+        </Box>
+
+        {inviteInput}
+
         <TextField
           fullWidth
           margin={'normal'}
           label={'Name'}
-          placeholder={'name of your group'}
           InputLabelProps={{
             shrink: true,
           }}
           value={name}
           onChange={onChange('name')}
         />
-        <PeaRadioGroup value={type} onChange={onChange('type')} />
+
         <TextField
-          multiline
-          rows={3}
-          classes={{
-            root: 'PeaFormControl-root',
-          }}
-          InputLabelProps={{
-            className: 'PeaFormLabel-root',
-          }}
-          InputProps={{
-            disableUnderline: true,
-            classes: {
-              root: 'PeaInput-root',
-              input: 'PeaInput-input',
-              formControl: 'PeaInput-formControl',
-            },
-          }}
-          FormHelperTextProps={{
-            className: 'PeaFormHelperText-root',
-          }}
           fullWidth
           margin={'normal'}
-          label={'Invited User'}
-          placeholder={'Type the name or username of the person in group'}
-          {...props}
+          label={'Description'}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={description || ''}
+          onChange={onChange('description')}
         />
-      </React.Fragment>
+
+        {typeInput}
+      </>
     }
     actions={[
       <PeaButton onClick={onClose}>Cancel</PeaButton>,
@@ -86,12 +111,18 @@ const PeaGroupDialog = ({
 PeaGroupDialog.metadata = {
   name: 'Pea Group Dialog',
 };
+
 PeaGroupDialog.propTypes = {
   dialogTitle: PropTypes.string,
   actionText: PropTypes.string,
   name: PropTypes.string.isRequired,
+  description: PropTypes.string,
   type: PropTypes.string.isRequired,
+  inviteInput: PropTypes.object.isRequired,
+  typeInput: PropTypes.object.isRequired,
+  profilePhoto: PropTypes.string,
   submitting: PropTypes.bool,
+  onChangeCoverPhotoClicked: PropTypes.func,
   onChange: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired,
@@ -101,6 +132,9 @@ PeaGroupDialog.defaultProps = {
   dialogTitle: 'Create Group',
   actionText: 'Create',
   submitting: false,
+  description: '',
+  profilePhoto: undefined,
+  onChangeCoverPhotoClicked: () => {},
 };
 
 export default PeaGroupDialog;
