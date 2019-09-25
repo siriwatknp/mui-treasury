fe045629a80ef14de6a14131318051411e2384dc
siriwatknp <siriwatkunaporn@gmail.com>
2019-09-25 22:11:38 +0800

    change babel config name to babel-config.js to prevent overriding when gatsby-build

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,12 +75,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     // trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
     // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
