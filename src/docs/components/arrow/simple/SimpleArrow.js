20a5a4fa22d58a0bc76a712cd71284088bab1483
siriwatknp <siriwatkunaporn@gmail.com>
2019-10-21 23:29:51 +0700

    change todo ref issue

--- src/docs/components/arrow/simple/SimpleArrow.js
+++ src/docs/components/arrow/simple/SimpleArrow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// @todo #166 move SimpleArrow from previous version.
+// @todo #1 move SimpleArrow from previous version.
 const SimpleArrow = () => <div>Simple arrow</div>;
 
 SimpleArrow.propTypes = {};
