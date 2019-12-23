243eaaea92b73571767ce1a3f840f9a11ff59e65
siriwatknp <siriwatkunaporn@gmail.com>
2019-12-23 22:05:38 +0700

    feat: 🎸 width can be any unit ex. %, em, rem

--- packages/mui-layout/src/__tests__/models/width.test.js
+++ packages/mui-layout/src/__tests__/models/width.test.js
@@ -2,27 +2,30 @@ import createWidth, { reduceWidths } from '../../models/width';
 
 describe('Width', () => {
   test('turn width to css auto', () => {
-    const width = createWidth('auto');
+    let width = createWidth('auto');
     expect(width.getStyle()).toEqual({ width: 'auto' });
-  });
 
-  test('turn width to css', () => {
-    const width = createWidth();
+    width = createWidth();
     expect(width.getStyle()).toBeUndefined();
-  });
 
-  test('turn width to css', () => {
-    const width = createWidth(200);
+    width = createWidth(200);
     expect(width.getStyle()).toEqual({ width: 'calc(100% - 200px)' });
   });
 
   test('reduce widths to css', () => {
-    const widths = [
-      createWidth(100),
-      createWidth(200),
-    ];
+    let widths = [createWidth(100), createWidth(200)];
     expect(reduceWidths(widths).getStyle()).toEqual({
       width: 'calc(100% - 300px)',
+    });
+
+    widths = [createWidth('25%'), createWidth(200)];
+    expect(reduceWidths(widths).getStyle()).toEqual({
+      width: 'calc(100% - (25% + 200px))',
+    });
+
+    widths = [createWidth('25%'), createWidth(200), createWidth('15em')];
+    expect(reduceWidths(widths).getStyle()).toEqual({
+      width: 'calc(100% - (25% + 200px + 15em))',
     });
   });
 });
