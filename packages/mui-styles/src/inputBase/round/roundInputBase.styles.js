2481e82a8313089083a4a49d7d25f9413fce7fdc
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2020-03-30 20:34:25 +0700

    feat: 🎸 support secondary color (#602)

--- packages/mui-styles/src/inputBase/round/roundInputBase.styles.js
+++ packages/mui-styles/src/inputBase/round/roundInputBase.styles.js
@@ -18,8 +18,9 @@ export default ({ palette }) => ({
   adornedEnd: {
     paddingRight: '0.5rem',
   },
+  colorSecondary: {},
   focused: {
-    boxShadow: '0 1px 4px 0 rgba(0,0,0,0.24)',
+    boxShadow: '0 2px 6px 0 rgba(0,0,0,0.24)',
     borderColor: palette.primary.main,
     '&:hover:not($disabled)': {
       borderColor: palette.primary.main,
@@ -29,6 +30,17 @@ export default ({ palette }) => ({
         color: palette.primary.light,
       },
     },
+    '&$colorSecondary': {
+      borderColor: palette.secondary.main,
+      '&:hover:not($disabled)': {
+        borderColor: palette.secondary.main,
+      },
+      '&$adornedStart': {
+        '& > svg': {
+          color: palette.secondary.light,
+        },
+      },
+    }
   },
   error: {
     backgroundColor: '#fff9f9',
