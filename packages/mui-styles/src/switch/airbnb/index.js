4ccf26320d36aadb748ec589f6ecd40b518afabc
siriwatknp <siriwatkunaporn@gmail.com>
2020-04-07 23:25:25 +0700

    feat: 🎸 #546 add AirbnbSwitch style
    ✅ Closes: #546

---
+++ packages/mui-styles/src/switch/airbnb/index.js
@@ -0,0 +1,8 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import airbnbSwitchStyles from './airbnbSwitch.styles';
+
+const useAirbnbSwitchStyles = makeStyles(airbnbSwitchStyles, { name: "AirbnbSwitch" });
+
+export { airbnbSwitchStyles, useAirbnbSwitchStyles };
+
+export { default } from './airbnbSwitch.styles';
