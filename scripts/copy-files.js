61946589f11a71f77c467b0615d25e2a1b58c31e
Siriwat Kunaporn <siriwatkunaporn@gmail.com>
2020-04-09 09:16:00 +0700

    fix: 🐛 add typescriptCopy to copy-files script (#649)
    ✅ Closes: #624

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -1,22 +1,24 @@
 /* eslint-disable no-console */
 const path = require('path');
 const fse = require('fs-extra');
+const glob = require('glob');
 
 const packagePath = process.cwd();
 const buildPath = path.join(packagePath, './build');
+const srcPath = path.join(packagePath, './src');
 
 async function createPackageFile() {
-  const packageData = await fse.readFile(
-    path.resolve(packagePath, './package.json'),
-    'utf8'
+  const packageData = await fse.readJson(
+    path.resolve(packagePath, './package.json')
   );
   const {
     nyc,
+    gitHead,
     scripts,
     devDependencies,
     workspaces,
     ...packageDataOther
-  } = JSON.parse(packageData);
+  } = packageData;
   const newPackageData = {
     ...packageDataOther,
     private: false,
@@ -24,11 +26,7 @@ async function createPackageFile() {
   };
   const targetPath = path.resolve(buildPath, './package.json');
 
-  await fse.writeFile(
-    targetPath,
-    JSON.stringify(newPackageData, null, 2),
-    'utf8'
-  );
+  await fse.writeJSON(targetPath, newPackageData, { spaces: 2 });
   console.log(`Created package.json in ${targetPath}`);
 
   return newPackageData;
@@ -40,9 +38,34 @@ async function copyReadMeFile() {
   return fse.copyFile(sourcePath, targetPath);
 }
 
+async function typescriptCopy({ from, to }) {
+  if (!(await fse.exists(to))) {
+    console.warn(`path ${to} does not exists`);
+    return [];
+  }
+
+  const files = glob.sync('**/*.d.ts', { cwd: from });
+  const cmds = files.map(file =>
+    fse.copy(path.resolve(from, file), path.resolve(to, file))
+  );
+  await Promise.all(cmds);
+
+  // add types to package.json in build folder
+  const targetPath = path.resolve(buildPath, './package.json');
+  const packageData = await fse.readJson(targetPath);
+  const newPackageData = {
+    ...packageData,
+    types: './index.d.ts',
+  };
+  await fse.writeJson(targetPath, newPackageData, { spaces: 2 });
+}
+
 async function run() {
   try {
     await Promise.all([createPackageFile(), copyReadMeFile()]);
+    if (process.env.DTS_INCLUDED === 'true') {
+      await typescriptCopy({ from: srcPath, to: buildPath });
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
