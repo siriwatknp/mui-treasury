0c6a7e9faae571942b1dff8f07e9b1d5717aba48
siriwatknp <siriwatkunaporn@gmail.com>
2020-04-28 21:33:12 +0700

    fix: 🐛 remove unused code

--- packages/mui-layout/src/core/index.ts
+++ packages/mui-layout/src/core/index.ts
@@ -1,6 +1,5 @@
 export { default } from '../builders';
 export * from './Context';
-export { setupStyled } from './styled';
 export { default as useSidebar } from './hooks/useSidebar';
 export { default as useSidebarCta } from './hooks/useSidebarCta';
 export { default as useInsetSidebar } from './hooks/useInsetSidebar';
