5e4660f95eb4076579525c4bf444707ea36df916
siriwatknp <siriwatkunaporn@gmail.com>
2020-05-01 23:16:19 +0700

    feat: 🎸 add autolink-header plugin

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -55,9 +55,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [".mdx", ".md"],
+        extensions: ['.mdx', '.md'],
         defaultLayouts: {
-          default: require.resolve("./src/templates/DocTemplate.js"),
+          default: require.resolve('./src/templates/DocTemplate.js'),
         },
         gatsbyRemarkPlugins: [
           {
@@ -73,6 +73,15 @@ module.exports = {
             resolve: 'gatsby-remark-embed-snippet',
             options: {
               directory: `${__dirname}/src/docs`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `80`,
+              className: 'anchor-header',
+              isIconAfterHeader: true,
+              icon: `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>`
             },
           },
           `gatsby-remark-prismjs`,
