703229f2db75299726ea085dd400df79b4492b2a
Paddy <pacurtin@tcd.ie>
2020-05-06 17:55:40 -0500

    #540 minimal select styling

---
+++ packages/mui-styles/src/select/minimal/index.js
@@ -0,0 +1,8 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import minimalSelectStyles from './minimalSelect.styles';
+
+const useMinimalSelectStyles = makeStyles(minimalSelectStyles, { name: "MinimalSelect" });
+
+export { minimalSelectStyles, useMinimalSelectStyles };
+
+export { default } from './minimalSelect.styles';
