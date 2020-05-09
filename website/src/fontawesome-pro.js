59753e03bd9d18aff4f6f8edcfaaa8993306a018
siriwatknp <siriwatkunaporn@gmail.com>
2020-05-09 12:30:04 +0700

    refactor: 💡 only add font-awesome pro in CI

---
+++ website/src/fontawesome-pro.js
@@ -0,0 +1,20 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faFighterJet,
+  faDiceD8,
+  faSparkles,
+  faCubes,
+  faCity,
+  faThumbsUp,
+} from '@fortawesome/pro-duotone-svg-icons';
+import { faEnvelope } from '@fortawesome/pro-solid-svg-icons';
+
+library.add(
+  faFighterJet,
+  faDiceD8,
+  faSparkles,
+  faCubes,
+  faCity,
+  faThumbsUp,
+  faEnvelope
+);
