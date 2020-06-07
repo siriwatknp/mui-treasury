c18381d44781fe073c83dfbcb9588281522678e6
siriwatknp <siriwatkunaporn@gmail.com>
2020-06-07 16:37:28 +0700

    fix: 🐛 add createStyles for typescript

--- _templates/style/ts/mui-styles.ejs.t
+++ _templates/style/ts/mui-styles.ejs.t
@@ -2,11 +2,12 @@
 to: packages/mui-styles/src/<%= category %>/<%= name %>/<%= name + h.changeCase.pascal(category) %>.styles.ts
 unless_exists: true
 ---
-<% type = h.changeCase.pascal(name) + h.changeCase.pascal(category) %><% fileName = name + h.changeCase.pascal(category) %>export type <%= type %>StyleProps = {}
+<% type = h.changeCase.pascal(name) + h.changeCase.pascal(category) %><% fileName = name + h.changeCase.pascal(category) %>import { createStyles } from '@material-ui/core/styles';
+export type <%= type %>StyleProps = {}
 
 export type <%= type %>ClassKey = keyof ReturnType<typeof <%= fileName %>Styles>
 
-const <%= fileName %>Styles = () => ({
+const <%= fileName %>Styles = () => createStyles({
 
 })
 
--- packages/mui-styles/src/avatar/grow/growAvatar.styles.ts
+++ packages/mui-styles/src/avatar/grow/growAvatar.styles.ts
@@ -1,3 +1,5 @@
+import { createStyles } from '@material-ui/core/styles';
+
 export type GrowAvatarStyleProps = {
   src: string;
   blur?: string;
@@ -8,36 +10,43 @@ export type GrowAvatarStyleProps = {
 
 export type GrowAvatarClassKey = keyof ReturnType<typeof growAvatarStyles>;
 
-const growAvatarStyles = () => ({
-  root: ({ src, blur = '8px', size, radius = '50%', opacity = 0.8 }: GrowAvatarStyleProps) => ({
-    position: 'relative',
-    zIndex: 0,
-    width: size,
-    height: size,
-    overflow: 'visible',
-    '& > div': {
+const growAvatarStyles = () =>
+  createStyles({
+    root: ({
+      src,
+      blur = '8px',
+      size,
+      radius = '50%',
+      opacity = 0.8,
+    }: GrowAvatarStyleProps) => ({
+      position: 'relative',
+      zIndex: 0,
       width: size,
       height: size,
-      borderRadius: radius,
-    },
-    '&:before': {
-      margin: -4,
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      zIndex: -1,
-      borderRadius: radius,
-      backgroundImage: `url(${src})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      filter: `blur(${blur})`,
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      opacity,
-    },
-  }),
-});
+      overflow: 'visible',
+      '& > div': {
+        width: size,
+        height: size,
+        borderRadius: radius,
+      },
+      '&:before': {
+        margin: -4,
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        zIndex: -1,
+        borderRadius: radius,
+        backgroundImage: `url(${src})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        filter: `blur(${blur})`,
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        opacity,
+      },
+    }),
+  });
 
 export default growAvatarStyles;
