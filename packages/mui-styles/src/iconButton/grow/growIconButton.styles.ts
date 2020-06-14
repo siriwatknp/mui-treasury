e4c4406258b2b2105f345e55681b365614f31653
siriwatknp <siriwatkunaporn@gmail.com>
2020-06-14 11:32:05 +0700

    fix: 🐛 ts error in growIconButton style

--- packages/mui-styles/src/iconButton/grow/growIconButton.styles.ts
+++ packages/mui-styles/src/iconButton/grow/growIconButton.styles.ts
@@ -1,21 +1,27 @@
-import { createStyles } from '@material-ui/core/styles';
+import { createStyles, Theme } from '@material-ui/core/styles';
 export type GrowIconButtonStyleProps = {
   color?: string;
   thickness?: number;
-}
+};
 
-export type GrowIconButtonClassKey = keyof ReturnType<typeof growIconButtonStyles>
+export type GrowIconButtonClassKey = keyof ReturnType<
+  typeof growIconButtonStyles
+>;
 
-const growIconButtonStyles = ({ palette }) => createStyles({
-  root: ({ color = palette.primary.light, thickness = 3 }) => ({
-    backgroundColor: '#fff',
-    boxShadow: 'none',
-    transition: '0.3s cubic-bezier(.47,1,.41,.8)',
-    '&:hover, &:focus': {
+const growIconButtonStyles = ({ palette }: Theme) =>
+  createStyles({
+    root: ({
+      color = palette.primary.light,
+      thickness = 3,
+    }: GrowIconButtonStyleProps) => ({
       backgroundColor: '#fff',
-      boxShadow: `0 0 0 ${thickness}px ${color}`,
-    }
-  })
-})
+      boxShadow: 'none',
+      transition: '0.3s cubic-bezier(.47,1,.41,.8)',
+      '&:hover, &:focus': {
+        backgroundColor: '#fff',
+        boxShadow: `0 0 0 ${thickness}px ${color}`,
+      },
+    }),
+  });
 
-export default growIconButtonStyles
+export default growIconButtonStyles;
