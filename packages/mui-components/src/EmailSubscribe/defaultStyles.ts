c9dc58c098c4c41424868f6f12a4409069d15976
siriwatknp <siriwatkunaporn@gmail.com>
2020-07-25 21:04:36 +0700

    style: 💄 update EmailSubscribe default style

--- packages/mui-components/src/EmailSubscribe/defaultStyles.ts
+++ packages/mui-components/src/EmailSubscribe/defaultStyles.ts
@@ -13,8 +13,12 @@ export const useDefaultStyles: (
 export default function defaultStyles({ palette, spacing }: Theme) {
   return createStyles({
     form: {
-      display: 'flex',
+      display: 'inline-flex',
       alignItems: 'center',
+      borderRadius: 4,
+      '&:focus-within': {
+        boxShadow: '0 0 12px 0 rgba(0,0,0,0.12)',
+      }
     },
     input: {
       backgroundColor: palette.background.paper,
@@ -23,13 +27,18 @@ export default function defaultStyles({ palette, spacing }: Theme) {
       border: '1px solid',
       borderColor: palette.text.hint,
       borderRightWidth: 0,
+      borderRadius: '4px 0 0 4px',
       padding: '0 0.5rem',
+      outline: 'none',
       '&:hover, &:focus': {
+        borderColor: palette.primary.main,
+        boxShadow: `inset 0 0 0 1px ${palette.primary.main}`,
         backgroundColor: palette.type === 'dark' ? palette.action.hover : '#f9f9f9',
       },
     },
     submit: {
       padding: spacing(1, 2),
+      borderRadius: '0 4px 4px 0',
       color: palette.common.white,
       backgroundColor: palette.primary.main,
       '&:hover, &:focus': {
