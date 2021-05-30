32617346f515dec1ad9ec63322a96df8fda62a8b
siriwatknp <siriwatkunaporn@gmail.com>
2021-05-30 18:18:19 +0700

    chore: fix no dist for types package

--- scripts/prerelease.ts
+++ scripts/prerelease.ts
@@ -4,13 +4,12 @@ import { set } from "edit-package-json";
 
 const PUBLISH_DIR = "dist";
 
-type PackageType = "cli" | "theme" | "component" | "style" | "layout";
+type PackageType = "cli" | "theme" | "component" | "style" | "layout" | "types";
 
 async function run() {
   // ts-node  prerelease.ts  component
   // 0      | 1            | 2
   const packageType = process.argv[2] as PackageType;
-  console.info("packageType", packageType);
 
   if (!packageType) {
     throw new Error("arg must not be undefined");
@@ -23,34 +22,31 @@ async function run() {
       (result) => fsp.writeFile(`${PUBLISH_DIR}/package.json`, result)
     );
   }
+  if (packageType === "types") {
+    await Promise.all([
+      cpy("index.d.ts", PUBLISH_DIR),
+      cpy("package.json", PUBLISH_DIR),
+    ]);
+  }
   if (["component", "style", "theme", "hook", "mockup"].includes(packageType)) {
     await Promise.resolve(file)
-      .then((newFile) => {
-        console.info('replacing "main"');
-        return set(newFile, "main", "index.js");
-      })
-      .then((newFile) => {
-        console.info('replacing "types"');
-        return set(newFile, "types", "index.d.ts");
-      })
-      .then((result) => {
-        console.info("writing file to dir:", PUBLISH_DIR);
-        return fsp.writeFile(`${PUBLISH_DIR}/package.json`, result);
-      });
+      .then((newFile) => set(newFile, "main", "index.js"))
+      .then((newFile) => set(newFile, "types", "index.d.ts"))
+      .then((result) => fsp.writeFile(`${PUBLISH_DIR}/package.json`, result));
   }
   if (packageType === "layout") {
     await fsp.writeFile(`${PUBLISH_DIR}/package.json`, file);
   }
-  // try {
-  //   await cpy("README.md", PUBLISH_DIR);
-  // } catch (error) {
-  //   console.log("error", error);
-  // }
-  // try {
-  //   await cpy("CHANGELOG.md", PUBLISH_DIR);
-  // } catch (error) {
-  //   console.log("error", error);
-  // }
+  try {
+    await cpy("README.md", PUBLISH_DIR);
+  } catch (error) {
+    console.log("error", error);
+  }
+  try {
+    await cpy("CHANGELOG.md", PUBLISH_DIR);
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 run().catch((error) => {
