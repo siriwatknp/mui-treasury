ae3ad7e3c33002c236956e836cf9d7dbc198ae64
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-11 00:26:33 +0700

    refactor(hook): introduce useSeparatorInput

--- packages/use-birthdate-input/src/useBirthdateInput.test.tsx
+++ packages/use-birthdate-input/src/useBirthdateInput.test.tsx
@@ -27,67 +27,30 @@ describe("useBirthdateInput", () => {
   it("should have these html attributes", () => {
     render(<BirthdateInput />);
 
-    expect(getInput()).toHaveAttribute("maxlength", "10");
     expect(getInput()).toHaveAttribute("size", "10");
-    expect(getInput()).toHaveAttribute("type", "tel");
+    expect(getInput()).toHaveAttribute("inputmode", "numeric");
   });
 
   it("receive only number", () => {
     render(<BirthdateInput />);
 
-    userEvent.type(getInput(), "abc");
+    userEvent.type(getInput(), "abc/.");
     expect(getInput()).toHaveValue("");
-  });
-
-  it("append separator and remove on backspace", () => {
-    render(<BirthdateInput />);
-
-    userEvent.type(getInput(), "10");
-    expect(getInput()).toHaveValue("10/");
-
-    userEvent.type(getInput(), "01");
-    expect(getInput()).toHaveValue("10/01/");
-
-    userEvent.type(getInput(), "1");
-    expect(getInput()).toHaveValue("10/01/1");
-
-    userEvent.type(getInput(), "{backspace}");
-    expect(getInput()).toHaveValue("10/01");
-
-    userEvent.type(getInput(), "{backspace}{backspace}");
-    expect(getInput()).toHaveValue("10");
-  });
-
-  it("insert separator at third and fifth position", () => {
-    render(<BirthdateInput defaultValue="10" />);
-
-    userEvent.type(getInput(), "0");
-    // insert separator
-    expect(getInput()).toHaveValue("10/0");
-
-    // setup
-    userEvent.type(getInput(), "1");
-    userEvent.type(getInput(), "1{backspace}");
-    expect(getInput()).toHaveValue("10/01");
-
-    userEvent.type(getInput(), "2");
-    // insert separator
-    expect(getInput()).toHaveValue("10/01/2");
   });
 
   describe("insert separator", () => {
     it("able to type separator at third", () => {
-      render(<BirthdateInput defaultValue="10" />);
+      render(<BirthdateInput defaultValue="1" />);
 
-      userEvent.type(getInput(), "/");
+      userEvent.type(getInput(), "0");
 
       expect(getInput()).toHaveValue("10/");
     });
 
     it("able to type separator at sixth", () => {
-      render(<BirthdateInput defaultValue="10/01" />);
+      render(<BirthdateInput defaultValue="10/0" />);
 
-      userEvent.type(getInput(), "/");
+      userEvent.type(getInput(), "1");
 
       expect(getInput()).toHaveValue("10/01/");
     });
--- packages/use-birthdate-input/src/useBirthdateInput.ts
+++ packages/use-birthdate-input/src/useBirthdateInput.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSeparatorInput } from "@mui-treasury/use-input-siblings";
 
 export interface UseBirthdateInputOptions {
   /**
@@ -23,60 +24,20 @@ interface InputHanders {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
-const someEqual = <T>(value: T, matches: Array<T>) =>
-  matches.some((match) => value === match);
 
 export const useBirthdateInput = (options?: UseBirthdateInputOptions) => {
-  const { separator = "/", defaultValue } = options || {};
-  const [value, setValue] = React.useState(defaultValue || "");
-  const codeRef = React.useRef<string | undefined>();
+  const result = useSeparatorInput({
+    ...options,
+    maxLength: [2, 2, 4],
+    validator: (value) => new RegExp(/(\d|\/)/).test(value.substr(-1)), // last char must be number or separator
+  });
   return {
+    ...result,
     getInputProps: (handlers?: InputHanders) => ({
-      maxLength: 10,
       size: 10,
       pattern: "[0-9]{2}.[0-9]{2}.[0-9]{4}",
-      type: "tel",
-      value,
-      onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-        handlers?.onChange?.(event);
-        const inputValue = event.target.value;
-        let newValue = inputValue;
-        if (
-          codeRef.current === "Backspace" &&
-          someEqual(inputValue.length, [2, 3, 5, 6])
-        ) {
-          newValue = inputValue.slice(0, -1);
-        } else {
-          const latestChar = inputValue.substr(-1);
-          if (
-            !!latestChar &&
-            latestChar !== separator &&
-            !/\d/.test(latestChar)
-          )
-            return; // should be separator or number
-          if (
-            latestChar !== separator &&
-            inputValue.length === 3 &&
-            inputValue[2] !== separator
-          ) {
-            newValue = `${inputValue.substr(0, 2)}${separator}${inputValue[2]}`;
-          } else if (
-            latestChar !== separator &&
-            inputValue.length === 6 &&
-            inputValue[4] !== separator
-          ) {
-            newValue = `${inputValue.substr(0, 5)}${separator}${inputValue[5]}`;
-          } else if (someEqual(inputValue.length, [2, 5])) {
-            newValue = `${inputValue}${separator}`;
-          }
-        }
-        setValue(newValue);
-        options?.onChange?.(newValue);
-      },
-      onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => {
-        handlers?.onKeyDown?.(event);
-        codeRef.current = event.code;
-      },
+      inputMode: "numeric" as const,
+      ...result.getInputProps(handlers),
     }),
   };
 };
