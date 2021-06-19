fb9e5e4212f7b1c2118bf07883f11d106e43d0bd
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-19 09:28:48 +0700

    fix(emailsubscribe): remove @material-ui/styles

--- packages/component-emailsubscribe/src/theme-extended.ts
+++ packages/component-emailsubscribe/src/theme-extended.ts
@@ -1,12 +1,19 @@
-import { StyleRules } from "@material-ui/core/styles";
+import { CSSInterpolation } from "@material-ui/system";
 import { EmailSubscribeFormProps } from "./EmailSubscribe";
 import { emailSubscribeClasses } from "./emailSubscribeClasses";
+
+type OverridesStyleRules<ClassKey extends string = string> = Record<
+  ClassKey,
+  CSSInterpolation
+>;
 
 declare module "@material-ui/core/styles/components" {
   interface Components {
     JunEmailSubscribe?: {
       defaultProps?: Partial<EmailSubscribeFormProps>;
-      styleOverrides?: Partial<StyleRules<keyof typeof emailSubscribeClasses>>;
+      styleOverrides?: Partial<
+        OverridesStyleRules<keyof typeof emailSubscribeClasses>
+      >;
     };
   }
 }
