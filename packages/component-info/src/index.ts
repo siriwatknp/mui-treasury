8cac11b85ec5c095737bb3147748b6f71445de04
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-23 21:35:56 +0700

    fix(component-info): types and dependency

--- packages/component-info/src/index.ts
+++ packages/component-info/src/index.ts
@@ -1,7 +1,7 @@
 export * from "./theme-extended";
 
 export * from "./Info";
-export { InfoPropsVariantOverrides } from "./Info";
+export type { InfoPropsVariantOverrides } from "./Info";
 export * from "./InfoHead";
 export * from "./InfoBody";
 export * from "./InfoEyebrow";
