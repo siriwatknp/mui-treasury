3170b5d9ce0b76a4ec29f1f6d69c17c4d0f96ed9
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-06 08:17:48 +0000

    Deploying to gh-pages from @ siriwatknp/mui-treasury@bc722d06c908b373b98517e7cb10ef20d260afb9 🚀

---
+++ sb-manager/index.js
@@ -0,0 +1,1 @@
+import{Provider,Root,renderStorybookUI}from"./chunk-7KC7JBAG.js";import"./chunk-V7GYLJLR.js";import"./chunk-SJD7EFYL.js";import"./chunk-JMPBYCHU.js";import"./chunk-6KNXZZZ5.js";import"./chunk-JSPL73I3.js";import"./chunk-GBCXFRBK.js";import"./chunk-DP4DR4LA.js";import"./chunk-Z4NRYNHR.js";export{Provider,Root,renderStorybookUI};
