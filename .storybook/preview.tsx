88fad41affac65988794034c2a1ca1354fc71318
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-12 01:02:06 +0700

    docs(layout): add installation page

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -38,6 +38,7 @@ const preview: Preview = {
           "Layout",
           [
             "Introduction",
+            "Installation",
             "Core Concept",
             "Starter Templates",
             "Migration to v5",
