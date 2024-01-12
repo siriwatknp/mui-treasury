3773dbd13b2101196ff9aa624bab7dfd7593c8de
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-12 09:44:52 +0700

    feat(menu): migrate menu download

---
+++ blocks/menu-download/MenuDownload.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import googleFont from "../../.storybook/decorators/googleFont";
+import storyDialog from "../../.storybook/decorators/storyDialog";
+import Usage from "./usage.mdx";
+import { MenuDownload } from "./index";
+
+const meta = {
+  title: "Menu/Download",
+  component: MenuDownload,
+  parameters: {
+    layout: "centered",
+    githubUsername: "siriwatknp", // (optional) Your github username. If provided, your avatar will be displayed in the story toolbar
+  },
+  decorators: [storyDialog(Usage), googleFont([])],
+} satisfies Meta<typeof MenuDownload>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Download: Story = {
+  render: () => <MenuDownload />,
+};
+
+
