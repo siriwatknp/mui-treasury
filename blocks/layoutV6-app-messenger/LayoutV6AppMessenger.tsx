1220d42a97c7f5251de4d2f38cc1adc60a7b3d9b
siriwatknp <siriwatkunaporn@gmail.com>
2024-05-29 11:56:11 -0400

    add layout v6 messenger

---
+++ blocks/layoutV6-app-messenger/LayoutV6AppMessenger.tsx
@@ -0,0 +1,145 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
+import {
+  createTheme,
+  responsiveFontSizes,
+  styled,
+  ThemeProvider,
+} from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import {
+  ChatBar,
+  ChatDialog,
+  ChatList,
+  ChatSettings,
+  ChatsHeader,
+  ConversationHead,
+  MessengerSearch,
+  // @ts-ignore
+} from "../layout-app-messenger/components";
+import {
+  Content,
+  EdgeDrawerClose,
+  EdgeSidebar,
+  Footer,
+  Header,
+  InsetAvoidingView,
+  InsetContainer,
+  InsetSidebar,
+  Root,
+  SidebarContent,
+  toggleEdgeSidebarDrawer,
+} from "../layout-v6-ditto";
+
+const StyledHeader = styled(Header)(() => ({
+  boxShadow: "0 1px 2px 0 rgba(0, 0, 0, .10)",
+  backgroundColor: "#ffffff",
+}));
+
+const IconButtonEdit = styled(IconButton)(() => ({
+  backgroundColor: "rgba(0,0,0,0.04)",
+}));
+
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: "rgb(0, 153, 255)",
+      },
+      background: {
+        default: "#fff",
+      },
+    },
+    typography: {
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      body1: {
+        fontSize: `${15 / 16}rem`,
+      },
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          "strong, b": {
+            fontWeight: "bold",
+          },
+        },
+      },
+    },
+  }),
+);
+// @ts-ignore
+theme.vars = null;
+
+export function LayoutV6AppMessenger() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Root>
+        <StyledHeader
+          position="relative"
+          sx={{
+            height: 64,
+            ".JunRoot:has(&)": {
+              "--Header-height": "64px",
+            },
+          }}
+        >
+          <Toolbar disableGutters sx={{ flex: "auto" }}>
+            <ConversationHead />
+          </Toolbar>
+        </StyledHeader>
+        <EdgeSidebar
+          sx={{
+            "--JunSidebar-permanentWidth": "25cqw",
+            "--JunSidebar-collapsible": {
+              xs: "var(--collapsed)",
+              md: "var(--uncollapsed)",
+            },
+          }}
+        >
+          <SidebarContent sx={{ borderRight: "1px solid rgba(0, 0, 0, 0.12)" }}>
+            <ChatsHeader />
+            <Box p={"4px 16px 12px"}>
+              <MessengerSearch />
+            </Box>
+            <ChatList />
+          </SidebarContent>
+        </EdgeSidebar>
+        <Content>
+          <InsetContainer
+            disableGutters
+            rightSidebar={
+              <InsetSidebar
+                position="absolute"
+                sx={{
+                  width: "25cqw",
+                  borderColor: "divider",
+                  ".JunRoot:has(&)": {
+                    "--InsetSidebarR-width": "25cqw",
+                  },
+                }}
+              >
+                <ChatSettings />
+              </InsetSidebar>
+            }
+          >
+            <ChatDialog />
+          </InsetContainer>
+        </Content>
+        <Footer>
+          <Container disableGutters>
+            <InsetAvoidingView>
+              <Box display={"flex"} alignItems={"center"} p={1}>
+                <ChatBar />
+              </Box>
+            </InsetAvoidingView>
+          </Container>
+        </Footer>
+      </Root>
+    </ThemeProvider>
+  );
+}
