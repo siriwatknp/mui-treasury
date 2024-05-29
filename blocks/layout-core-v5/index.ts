97192bc7817b67f9d5b3d356c68352802271e7dd
siriwatknp <siriwatkunaporn@gmail.com>
2024-05-29 11:03:03 -0400

    wip

--- blocks/layout-core-v5/index.ts
+++ blocks/layout-core-v5/index.ts
@@ -1,12 +1,12 @@
-export * from "./Root/Root";
-export * from "./Header/Header";
-export * from "./EdgeSidebar/EdgeSidebar";
-export * from "./EdgeSidebar/EdgeTrigger";
+export * from "./Root/Root"; // [x]
+export * from "./Header/Header"; // [x]
+export * from "./EdgeSidebar/EdgeSidebar"; // [x]
+export * from "./EdgeSidebar/EdgeTrigger"; // => EdgePermanentCollapse, EdgeDrawerClose
 export * from "./InsetSidebar/InsetSidebar";
 export * from "./InsetSidebar/InsetContainer";
 export * from "./InsetSidebar/InsetAvoidingView";
-export * from "./Content/Content";
-export * from "./Content/SidebarContent";
+export * from "./Content/Content"; // [x]
+export * from "./Content/SidebarContent"; // [x]
 export * from "./Footer/Footer";
 export * from "./Fullscreen/Fullscreen";
 export * from "./WindowContext";
--- blocks/layout-v6-ditto/DittoLayout.tsx
+++ blocks/layout-v6-ditto/DittoLayout.tsx
@@ -7,7 +7,9 @@ const PART = {
   HEADER: "JunHeader",
   SIDEBAR: "JunSidebar",
   SIDEBAR_R: "JunSidebarRight",
+  INSET_SIDEBAR: "JunInsetSidebar",
   MAIN: "JunMain",
+  FOOTER: "JunFooter",
 };
 
 export function collapseEdgeSidebar(options?: {
@@ -55,18 +57,38 @@ export function toggleEdgeSidebarDrawer(options?: {
     if (nextOpen) {
       sidebar.setAttribute("data-drawer-open", "");
       page.style.setProperty("--JunSidebar-drawerOpen", "1");
+      doc.body.style.overflowY = "hidden";
+      // @ts-ignore
+      function handleOutsideClick(event: MouseEvent) {
+        if (event.target === sidebar) {
+          // clicking on the backdrop (psuedo element of sidebar) will close the sidebar
+          toggleEdgeSidebarDrawer({
+            ...options,
+            state: false,
+          });
+          doc.removeEventListener?.("click", handleOutsideClick);
+        }
+      }
+      setTimeout(() => {
+        // prevent the `handleOutsideClick` to be called immediately
+        doc.addEventListener?.("click", handleOutsideClick);
+      }, 0);
+
+      // TODO: add a way to close the sidebar by swiping
+      // TODO: add a way to close the sidebar by pressing ESC
     } else {
       sidebar.removeAttribute("data-drawer-open");
       sidebar.setAttribute("data-mobile-closing", "");
       setTimeout(() => {
         sidebar.removeAttribute("data-mobile-closing");
       }, 300);
+      doc.body.style.overflowY = "";
       page.style.setProperty("--JunSidebar-drawerOpen", "");
     }
   }
 }
 
-export function EdgeDrawerClose() {
+export function EdgeDrawerClose({ sidebarId }: { sidebarId?: string }) {
   return (
     <Box
       component="button"
@@ -74,7 +96,8 @@ export function EdgeDrawerClose() {
         display: "none",
         position: "fixed",
         top: "0.875rem",
-        right: "0.875rem",
+        right: "var(--anchorLeft, 0.875rem)",
+        left: "var(--anchorRight, 0.875rem)",
         zIndex: 2,
         width: 40,
         height: 40,
@@ -94,6 +117,7 @@ export function EdgeDrawerClose() {
       onClick={() =>
         toggleEdgeSidebarDrawer({
           state: false,
+          sidebarId,
           document: (
             document.getElementById(
               "storybook-preview-iframe",
@@ -211,50 +235,56 @@ export function EdgePermanentCollapse() {
   );
 }
 
-export function Root({ className, ...props }: BoxProps) {
+export function Root({ className, sx, ...props }: BoxProps) {
   return (
     <Box
       {...props}
       className={`JunRoot ${className}`}
-      sx={{
-        bgcolor: "background.paper",
-        minHeight: "100lvh",
-        display: "grid",
-        position: "relative",
-        containerType: "size",
-        containerName: "page",
-        transition: "grid-template-columns 0.3s",
-        gridTemplateAreas: `
-        "${PART.HEADER}"
-        "${PART.MAIN}"
-        `,
-        gridTemplateRows: "auto 1fr",
+      sx={[
+        {
+          // overflow: "hidden auto",
+          bgcolor: "background.paper",
+          minHeight: "100lvh",
+          display: "grid",
+          position: "relative",
+          transition: "grid-template-columns 0.3s",
+          gridTemplateAreas: `
+          "${PART.HEADER}"
+          "${PART.MAIN}"
+          "${PART.FOOTER}"
+          `,
+          gridTemplateRows: "auto 1fr",
 
-        [`&:has(.${PART.SIDEBAR})`]: {
-          gridTemplateAreas: `
-            "${PART.SIDEBAR} ${PART.HEADER}"
-            "${PART.SIDEBAR} ${PART.MAIN}"
-          `,
-          gridTemplateColumns: "max-content 1fr",
-          "--JunSidebar-drawerOpen": "0",
+          [`&:has(.${PART.SIDEBAR})`]: {
+            gridTemplateAreas: `
+              "${PART.SIDEBAR} ${PART.HEADER}"
+              "${PART.SIDEBAR} ${PART.MAIN}"
+              "${PART.SIDEBAR} ${PART.FOOTER}"
+            `,
+            gridTemplateColumns: "max-content 1fr",
+            "--JunSidebar-drawerOpen": "0",
+          },
+          [`&:has(.${PART.SIDEBAR_R})`]: {
+            gridTemplateAreas: `
+              "${PART.HEADER} ${PART.SIDEBAR_R}"
+              "${PART.MAIN} ${PART.SIDEBAR_R}"
+              "${PART.FOOTER} ${PART.SIDEBAR_R}"
+            `,
+            gridTemplateColumns: "1fr max-content",
+            "--JunSidebar-drawerOpen": "0",
+          },
+          // TODO: find a better way to support both L & R
+          [`&:has(.${PART.SIDEBAR}):has(.${PART.SIDEBAR_R})`]: {
+            gridTemplateAreas: `
+              "${PART.SIDEBAR} ${PART.HEADER} ${PART.SIDEBAR_R}"
+              "${PART.SIDEBAR} ${PART.MAIN} ${PART.SIDEBAR_R}"
+              "${PART.SIDEBAR} ${PART.FOOTER} ${PART.SIDEBAR_R}"
+            `,
+            gridTemplateColumns: "max-content 1fr max-content",
+          },
         },
-        [`&:has(.${PART.SIDEBAR_R})`]: {
-          gridTemplateAreas: `
-            "${PART.HEADER} ${PART.SIDEBAR_R}"
-            "${PART.MAIN} ${PART.SIDEBAR_R}"
-          `,
-          gridTemplateColumns: "1fr max-content",
-          "--JunSidebar-drawerOpen": "0",
-        },
-        // TODO: find a better way to support both L & R
-        [`&:has(.${PART.SIDEBAR}):has(.${PART.SIDEBAR_R})`]: {
-          gridTemplateAreas: `
-            "${PART.SIDEBAR} ${PART.HEADER} ${PART.SIDEBAR_R}"
-            "${PART.SIDEBAR} ${PART.MAIN} ${PART.SIDEBAR_R}"
-          `,
-          gridTemplateColumns: "max-content 1fr max-content",
-        },
-      }}
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
     />
   );
 }
@@ -273,9 +303,6 @@ export const Header = ({ className = "", ...props }: BoxProps<"header">) => {
 
 const ContentRoot = styled("main")({
   gridArea: PART.MAIN,
-  bgcolor: "#fff",
-  borderTopLeftRadius: 20,
-  p: 2,
 });
 export function Content({ className, ...props }: BoxProps) {
   return <ContentRoot className={`${PART.MAIN} ${className}`} {...props} />;
@@ -292,9 +319,11 @@ const SidebarContentRoot = styled("div")(({ theme }) => ({
                var(--permanent, visible)` as any,
   overflowX: "auto", // prevent horizontal content overflow
   flex: 1,
-  position: "relative",
+  position: "var(--drawer, fixed) var(--permanent, relative)" as any,
   zIndex: 2,
   width: "var(--SidebarContent-width)",
+  height: "var(--drawer, 100vh)",
+  overflowY: "var(--drawer, auto)" as any,
   transition: `var(--drawer, opacity 0.3s, transform 0.3s)
                var(--permanent, opacity 0.7s, width 0.3s var(--SidebarContent-transitionDelay, 0s), box-shadow 0.3s var(--SidebarContent-transitionDelay, 0s))`,
   transform: `var(--drawer, var(--anchorLeft, translateX(calc((1 - var(--JunSidebar-drawerOpen)) * -100%))) var(--anchorRight, translateX(calc(var(--JunSidebar-drawerOpen) * -100%))))
@@ -313,9 +342,14 @@ const SidebarContentRoot = styled("div")(({ theme }) => ({
   },
 }));
 export function SidebarContent({ className = "", ...props }: BoxProps) {
+  const ref = React.useRef<HTMLDivElement>(null);
   return (
     // @ts-ignore
-    <SidebarContentRoot {...props} className={`SidebarContent ${className}`} />
+    <SidebarContentRoot
+      ref={ref}
+      {...props}
+      className={`SidebarContent ${className}`}
+    />
   );
 }
 
@@ -362,6 +396,9 @@ const EdgeSidebarRoot = styled("div")({
     opacity: "var(--JunSidebar-drawerOpen, 0)",
     visibility: "hidden",
   },
+  "&:not([data-drawer-open], [data-mobile-closing])": {
+    overflow: "var(--drawer, hidden)",
+  },
   "&[data-drawer-open]": {
     "&::before": {
       visibility: "visible",
@@ -372,6 +409,12 @@ const EdgeSidebarRoot = styled("div")({
   },
   "&[data-collapsible='uncollapsed']": {
     "--JunSidebar-collapsible": "var(--uncollapsed)",
+  },
+  ".JunRoot:has(&) .EdgeSidebar-trigger": {
+    display: "none",
+  },
+  ".JunRoot:has(&) .EdgeSidebarR-trigger": {
+    display: "none",
   },
 });
 export function EdgeSidebar({
@@ -398,7 +441,7 @@ const InsetContainerRoot = styled(Container)({
   display: "flex",
   flexFlow: "row nowrap",
   flexGrow: 1,
-  '& > *:not([class*="AppInsetSidebar"])': {
+  [`& > *:not([class*="${PART.INSET_SIDEBAR}"])`]: {
     flexGrow: 1,
     overflow: "auto",
   },
@@ -421,38 +464,92 @@ export function InsetContainer({
   );
 }
 
-const InsetSidebarRoot = styled("div")({
+export function InsetAvoidingView(props: BoxProps) {
+  return (
+    <Box
+      className="InsetAvoidingView"
+      {...props}
+      sx={{
+        transition: "all 225ms",
+        marginRight: "var(--InsetSidebarR-width)",
+        marginLeft: "var(--InsetSidebarL-width)",
+      }}
+    />
+  );
+}
+
+const InsetSidebarRoot = styled("aside")({
+  "--InsetSidebar-position": "var(--sticky)",
+  "--InsetSidebar-anchor": "var(--anchor-right)",
+  /** DO NOT OVERRIDE, internal variables */
+  "--sticky": "var(--InsetSidebar-position,)",
+  "--fixed": "var(--InsetSidebar-position,)",
+  "--absolute": "var(--InsetSidebar-position,)",
+  "--anchor-right": "var(--InsetSidebar-anchor,)",
+  "--anchor-left": "var(--InsetSidebar-anchor,)",
   position: "relative",
   flexShrink: 0,
 });
-const InsetSidebarBody = styled("div")<{
-  ownerState: { anchor?: "left" | "right" };
-}>(({ theme }) => ({
+const InsetSidebarBody = styled("div")(({ theme }) => ({
+  boxSizing: "content-box",
   display: "flex",
   flexDirection: "column",
+  backgroundColor: "inherit",
   overflow: "auto",
   borderColor: theme.palette.divider,
-  variants: [
-    {
-      props: { anchor: "left" },
-      style: {
-        borderRight: "1px solid",
-      },
-    },
-    {
-      props: { anchor: "right" },
-      style: {
-        borderLeft: "1px solid",
-      },
-    },
-  ],
+  position:
+    "var(--sticky, sticky) var(--fixed, fixed) var(--absolute, absolute)" as any,
+  height:
+    "var(--sticky, initial) var(--fixed, calc(100% - var(--Header-height, 0px))) var(--absolute, calc(100vh - var(--Header-height, 0px)))",
+  width: "inherit",
+  top: 0,
+  borderRight: "var(--anchor-left, 1px solid transparent)",
+  borderLeft: "var(--anchor-right, 1px solid transparent)",
+  marginLeft:
+    "var(--fixed, var(--anchor-left, -9999px)) var(--absolute, initial) var(--sticky, initial)",
+  paddingLeft:
+    "var(--fixed, var(--anchor-left, 9999px)) var(--absolute, initial) var(--sticky, initial)",
+  marginRight:
+    "var(--fixed, var(--anchor-right, -9999px)) var(--absolute, initial) var(--sticky, initial)",
+  paddingRight:
+    "var(--fixed, var(--anchor-right, 9999px)) var(--absolute, initial) var(--sticky, initial)",
+  paddingTop: "var(--fixed, var(--Header-height))",
 }));
-export function InsetSidebar(
-  props: React.PropsWithChildren<React.DetailsHTMLAttributes<HTMLDivElement>>,
-) {
+export function InsetSidebar({
+  position = "sticky",
+  anchor = "right",
+  children,
+  ...props
+}: {
+  anchor?: "left" | "right";
+  position?: "fixed" | "absolute" | "sticky";
+} & React.PropsWithChildren<React.DetailsHTMLAttributes<HTMLDivElement>>) {
   return (
-    <InsetSidebarRoot>
-      <InsetSidebarBody {...props} ownerState={{ anchor: "left" }} />
+    <InsetSidebarRoot
+      className={PART.INSET_SIDEBAR}
+      {...props}
+      style={{
+        ...(anchor !== "right" && {
+          "--InsetSidebar-anchor": `var(--anchor-${anchor})`,
+        }),
+        ...(position !== "sticky" && {
+          "--InsetSidebar-position": `var(--${position})`,
+        }),
+      }}
+    >
+      <InsetSidebarBody>{children}</InsetSidebarBody>
     </InsetSidebarRoot>
+  );
+}
+
+export function Footer(props: BoxProps) {
+  return (
+    <Box
+      component="footer"
+      {...props}
+      sx={{
+        transition: "all 225ms cubic-bezier(0.0, 0, 0.2, 1) 0ms, color 0s",
+      }}
+    />
   );
 }
