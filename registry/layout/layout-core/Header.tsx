b69973014409ba7d3c18e38459a9caded32ab61e
siriwatknp <siriwatkunaporn@gmail.com>
2026-02-19 08:40:28 +0700

    finish header

--- registry/layout/layout-core/Header.tsx
+++ registry/layout/layout-core/Header.tsx
@@ -1,63 +1,87 @@
 "use client";
-import React from "react";
-import { BoxProps } from "@mui/material/Box";
-import { Breakpoint } from "@mui/material/styles";
+import React, { useMemo } from "react";
+import { Breakpoint, SxProps, Theme } from "@mui/material/styles";
+import { unstable_memoTheme as memoTheme } from "@mui/material/utils";
 import { styled } from "@mui/material/styles";
 
-export function applyHeaderStyles(params?: {
-  height: string | Partial<Record<Breakpoint, string>>;
+interface HeaderProps {
+  height?: string | Partial<Record<Breakpoint, string>>;
   fullWidth?: boolean | Breakpoint;
-}) {
-  const { height, fullWidth } = params || {};
-  const clip = `
+  sx?: SxProps<Theme>;
+  className?: string;
+}
+
+const FULL_WIDTH_CLIP = `
     "Header Header Header"
     "EdgeSidebar Content EdgeSidebar-R"
     "EdgeSidebar Footer EdgeSidebar-R"
   `;
-  return {
-    height,
-    ".Root:has(&)": {
-      "--Header-height": height,
-      ...(fullWidth && {
-        gridTemplateAreas:
-          typeof fullWidth === "string"
-            ? {
-                [fullWidth]: clip,
-              }
-            : clip,
-        "--Header-clipHeight":
-          typeof fullWidth === "string"
-            ? {
-                [fullWidth]: "var(--Header-height)",
-              }
-            : "var(--Header-height)",
-      }),
-    },
-  };
-}
 
-const StyledHeader = styled("header")({
-  gridArea: "Header",
-  minHeight: 56,
-  alignContent: "center",
-  display: "flex",
-  alignItems: "center",
-  zIndex: 1,
-  top: 0,
-  position: "sticky",
-  background: "var(--Header-background)",
-  borderBottom: "var(--Header-underline)",
-});
+const StyledHeader = styled("header", {
+  name: "LayoutHeader",
+  slot: "root",
+})<{
+  ownerState: HeaderProps;
+}>(
+  memoTheme(({ theme }) => ({
+    gridArea: "Header",
+    minHeight: 56,
+    alignContent: "center",
+    display: "flex",
+    alignItems: "center",
+    zIndex: 1,
+    top: 0,
+    position: "sticky",
+    background: "var(--Header-background)",
+    borderBottom: "var(--Header-underline)",
+    height: "var(--Header-height)",
+    variants: [
+      {
+        props: ({ height }: HeaderProps) => !!height,
+        style: ({ height }: Required<HeaderProps>) =>
+          theme.unstable_sx({
+            ".Root:has(&)": {
+              "--Header-height": height,
+            },
+          }),
+      },
+      {
+        props: ({ fullWidth }: HeaderProps) => !!fullWidth,
+        style: ({ fullWidth }: Required<HeaderProps>) =>
+          theme.unstable_sx({
+            ".Root:has(&)": {
+              gridTemplateAreas:
+                typeof fullWidth === "string"
+                  ? { [fullWidth]: FULL_WIDTH_CLIP }
+                  : FULL_WIDTH_CLIP,
+              "--Header-clipHeight":
+                typeof fullWidth === "string"
+                  ? { [fullWidth]: "var(--Header-height)" }
+                  : "var(--Header-height)",
+            },
+          }),
+      },
+    ],
+  })),
+);
 
-const Header = React.forwardRef<HTMLElement, BoxProps>(function Header(
-  { className, ...props },
-  ref,
-) {
+const Header = React.forwardRef<
+  HTMLElement,
+  React.PropsWithChildren<HeaderProps>
+>(function Header({ className, height, fullWidth, sx, ...props }, ref) {
+  const ownerState = useMemo(
+    () => ({
+      height,
+      fullWidth,
+    }),
+    [height, fullWidth],
+  );
   return (
-    // @ts-expect-error BoxProps on styled native element
     <StyledHeader
       ref={ref}
       className={`Header ${className || ""}`}
+      ownerState={ownerState}
+      sx={sx}
       {...props}
     />
   );
