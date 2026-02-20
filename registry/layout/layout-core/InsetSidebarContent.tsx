05e8b14a2f163bde040babce6653d7d655c0f160
siriwatknp <siriwatkunaporn@gmail.com>
2026-02-20 10:40:44 +0700

    refactor(layout-core): simplify InsetSidebarContent to 2-toggle system
    - Remove --sticky toggle, sticky is now default fallback (handled by parent)
    - Only use --fixed and --absolute CSS variable toggles
    - Update absolute height to var(--jun-IC-absolute-h, 100%)

--- registry/layout/layout-core/InsetSidebarContent.tsx
+++ registry/layout/layout-core/InsetSidebarContent.tsx
@@ -11,24 +11,24 @@ const InsetSidebarContentRoot = styled("div")(({ theme }) => ({
   overflow: "auto",
   background: "var(--InsetSidebarContent-background)",
   boxSizing:
-    "var(--sticky, border-box) var(--fixed, content-box) var(--absolute, border-box)" as never,
+    "var(--fixed, content-box) var(--absolute, border-box)" as never,
   position:
-    "var(--sticky, sticky) var(--fixed, fixed) var(--absolute, absolute)" as never,
+    "var(--fixed, fixed) var(--absolute, absolute)" as never,
   height:
-    "var(--sticky, initial) var(--fixed, calc(100% - var(--jun-H-h, 0px))) var(--absolute, calc(var(--jun-h, 100vh) - var(--jun-H-h, 0px)))",
-  width: "var(--sticky, inherit) var(--fixed, inherit) var(--absolute, 100%)",
+    "var(--fixed, calc(100% - var(--jun-H-h, 0px))) var(--absolute, var(--jun-IC-absolute-h, 100%))",
+  width: "var(--fixed, inherit) var(--absolute, 100%)",
   top: 0,
   borderRight: "var(--anchor-left, var(--InsetSidebarContent-sideline))",
   borderLeft: "var(--anchor-right, var(--InsetSidebarContent-sideline))",
   borderColor: theme.palette.divider,
   marginLeft:
-    "var(--fixed, var(--anchor-left, -9999px)) var(--absolute, initial) var(--sticky, initial)",
+    "var(--fixed, var(--anchor-left, -9999px)) var(--absolute, initial)",
   paddingLeft:
-    "var(--fixed, var(--anchor-left, 9999px)) var(--absolute, initial) var(--sticky, initial)",
+    "var(--fixed, var(--anchor-left, 9999px)) var(--absolute, initial)",
   marginRight:
-    "var(--fixed, var(--anchor-right, -9999px)) var(--absolute, initial) var(--sticky, initial)",
+    "var(--fixed, var(--anchor-right, -9999px)) var(--absolute, initial)",
   paddingRight:
-    "var(--fixed, var(--anchor-right, 9999px)) var(--absolute, initial) var(--sticky, initial)",
+    "var(--fixed, var(--anchor-right, 9999px)) var(--absolute, initial)",
   marginTop: "var(--fixed, var(--jun-H-h))",
 }));
 
